4f89183a92bebda877ebd87b1b67eba8f3aab29c
Younghwan Jang <yfaney@gmail.com>
2016-04-26 13:52:48 -0500

    Adding MyPacket module
    Encoding and decoding BGP packet into byte array

--- MyPacket.py
+++ MyPacket.py
@@ -2,15 +2,24 @@ import struct
 import ctypes
 import binascii
 
+SIZE_HEADER = 4
+SIZE_ADDRESS = 4
+SIZE_SUBNET = 4
+SIZE_PATH = 4
+
+VALUE_RESERVED = 0
+
 KEY_NET = "net"
 KEY_SUB = "sub"
 KEY_PATH = "path"
 
-def encode(netString, subnString, pathList):
+def encode(dataType, netString, subnString, pathList):
     netArray = encodeAddr(netString)
     subArray = encodeAddr(subnString)
-    buf = ctypes.create_string_buffer(4+4+4*len(pathList))
+    buf = ctypes.create_string_buffer(SIZE_HEADER+SIZE_ADDRESS+SIZE_SUBNET+SIZE_PATH*len(pathList))
     offset = 0
+    struct.pack_into("BBBB", buf, offset, dataType, VALUE_RESERVED, VALUE_RESERVED, VALUE_RESERVED)
+    offset += 4
     struct.pack_into("BBBB", buf, offset, netArray[0], netArray[1], netArray[2], netArray[3])
     offset += 4
     struct.pack_into("BBBB", buf, offset, subArray[0], subArray[1], subArray[2], subArray[3])
@@ -20,14 +29,15 @@ def encode(netString, subnString, pathList):
     return buf.raw
 
 def decode(rawData):
-    if(len(rawData) < 12):
-        return -1
-    netString = decodeAddr(struct.unpack("BBBB", rawData[0:4]))
-    subString = decodeAddr(struct.unpack("BBBB", rawData[4:8]))
+    if(len(rawData) < 16):
+        return (-1, -1, -1, -1)
+    dataType = struct.unpack("B", rawData[0])
+    netString = decodeAddr(struct.unpack("BBBB", rawData[4:8]))
+    subString = decodeAddr(struct.unpack("BBBB", rawData[8:12]))
     pathList = []
-    for i in range(8, len(rawData), 4):
+    for i in range(12, len(rawData), 4):
         pathList.append(struct.unpack("=I", rawData[i:i+4])[0])
-    return (netString, subString, pathList)
+    return (dataType, netString, subString, pathList)
 
 def encodeAddr(addrString):
     addrArray = []
--- mybgp.py
+++ mybgp.py
@@ -3,11 +3,12 @@ import sys
 import socket               # Import socket module
 import thread
 import time
+import MyPacket
 
 #Global constants
 
 PORT_NUM = 57777                # Reserve a port for your service.
-INTERVAL = 60
+INTERVAL = 30
 
 # argv[0] = this program name
 # argv[1] = autonomous system
@@ -19,19 +20,21 @@ INTERVAL = 60
 #Global variables
 routingTable = []		# Routing Table!! You might want to save all ip address and AS number mappings
 
-autoSys = sys.argv[1]
-thisNet = sys.argv[2]
-thisSub = sys.argv[3]
-neighbors = sys.argv[4:]
 
 
+KEY_TYPE_REQUEST = 1
 
 # Actual BGP server module. Handles TCP receiving data
 def server_bgp(threadName, conn, addr):
     while True:
         data = conn.recv(1024)
-        if data[:3] == "REQ":
-            print "Received: %s" %(data)
+        if(len(data) > 13):
+            (dataType, network, subnet, pathVector) = MyPacket.decode(data)
+            if dataType == KEY_TYPE_REQUEST:
+                if determineLoop(pathVector):
+                    print "Loop detected. Ignoring..."
+                    continue
+                print "Received: net:%s sub:%s path:%s" %(network, subnet, pathVector)
     conn.close()
 
 # TCP Listening module. When connecting, it makes another thread and pass the connection to server_bgp() function
@@ -48,8 +51,6 @@ def server_listen(threadName):
             thread.start_new_thread( server_bgp, ("Thread-SV-BGP", conn, addr) )
         except:
             print "Error: unable to start server thread"
-
-
     s.close()                # Close the connection
 
 # BGP client module. It connects to the neighbor's IP.
@@ -58,9 +59,12 @@ def client_bgp(threadName, neighbor):
     print "Connecting to %s..." % neighbor
     cs.connect((neighbor, PORT_NUM))
     while(True):
-        cs.send(makePacketTest())
+        cs.send(MyPacket.encode(thisNet, thisSub, [autoSys]))
         time.sleep(INTERVAL)
     cs.close()
+
+def determineLoop(pathVector):
+    return autoSys in pathVector
 
 # For the testing for now. It sends its AS number and its network(not real IP. for simulation)
 def makePacketTest():
@@ -71,11 +75,14 @@ def usage():
     print "Example: %s 60001 192.168.10.0 255.255.255.0 10.0.0.1 10.0.1.2 ..." %(sys.argv[0])
 
 # Below is the main function!
-
 if(len(sys.argv) <4):
     usage()
     exit(1)
 
+autoSys = int(sys.argv[1])
+thisNet = sys.argv[2]
+thisSub = sys.argv[3]
+neighbors = sys.argv[4:]
 # Starting Server Listening Thread
 
 print "Starting Server Thread..."
